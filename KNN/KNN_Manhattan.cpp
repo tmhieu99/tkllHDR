4f7dd7fc1f68e059dceda100fbf178301f75d2eb
Nguyen Minh Dang <dang.nguyen.bkit@hcmut.edu.vn>
2019-04-10 22:41:07 +0700

    Major update
    1. Merge all small codes of KNN into one big program (With all data reading, error estimating, etc. features)
    2. Add MNIST dataset (.zip files)

--- KNN/KNN_Manhattan.cpp
+++ KNN/KNN_Manhattan.cpp
@@ -1,54 +1,96 @@
+// K-Nearest Neighbor using Euclidean distance on digit recognition
+// Dataset used: MNIST dataset
+
 #include <bits/stdc++.h>
 using namespace std;
 
-const string IMAGES_DATA    = "../images.txt";
-const string LABELS_DATA    = "../labels.txt";
-const string FILE_OUT       = "KNN_Manhattan_output.txt";
-const int MAXN              = 10000;
-const int S_TEST            = 0;
-const int T_TEST            = 1000;
-const int S_TRAINING        = 1000;
-const int T_TRAINING        = MAXN;
-const int INF               = 255*255 + 1;
 
-ofstream out(FILE_OUT);
 
-int images[MAXN][28*28];
-int labels[MAXN];
+// Global constants
+#define IMAGES_DATA     "train-images-idx3-ubyte"
+#define LABELS_DATA     "train-labels-idx1-ubyte"
+#define RESULT_OUT      "result_M.txt"
+#define DETAILS_OUT     "details_M.txt"
+#define MAXN            10000                   // Size of the dataset 
+#define TRAIN_SET_SIZE  ((int)(MAXN*0.9))       // 90% of the dataset
+//#define TEST_SET_SIZE   MAXN - TRAIN_SET_SIZE   // The rest
+#define TEST_SET_SIZE   10
+#define S_TEST          0
+#define T_TEST          TEST_SET_SIZE
+#define S_TRAINING      TEST_SET_SIZE
+#define T_TRAINING      TEST_SET_SIZE + TRAIN_SET_SIZE
+#define INF             65026                   // = 255*255 + 1
+
+
+
+// Global variables
+ofstream out(RESULT_OUT);
+ofstream details(DETAILS_OUT);
+unsigned char* images;
+unsigned char* labels;
+int correct = 0;
+
+
+
+// Prototypes
+void readData();                        // readData: Load data from MNIST dataset
+void showImage(ostream &out, int n);    // showImage: Print greyscale matrix of the n-th image
+int  showLabel(int n);                  // showLabel: Print the corresponding label of the n-th image
+void KNN();                             // KNN: Main K-nearest neighbor algorithm
+void reportResult(ostream &out);        // reportResult: Show statistical result
+
+
 
 void readData(){
-    ifstream img_in(IMAGES_DATA);
-    ifstream lbl_in(LABELS_DATA);
+    // Open images and labels data files
+    ifstream iinp(IMAGES_DATA, ios::binary);
+    ifstream linp(LABELS_DATA, ios::binary);
 
-    for (int n = 0 ; n < MAXN ; ++n){
-        for (int i = 0 ; i < 28*28 ; ++i){
-            img_in >> images[n][i];
-        }
-        lbl_in >> labels[n];
-    }
+    // Skip over MNIST headers
+    iinp.seekg(sizeof(int)*4);
+    linp.seekg(sizeof(int)*2);
 
-    img_in.close();
-    lbl_in.close();
+    // Read MNIST data
+    images = new unsigned char [MAXN*28*28];
+    labels = new unsigned char [MAXN];
+    iinp.read((char*)images, sizeof(unsigned char)*MAXN*28*28);
+    linp.read((char*)labels, sizeof(unsigned char)*MAXN);
 }
-void showImage(int n){
+
+void showImage(ostream &out, int n){
     for (int i = 0 ; i < 28 ; ++i){
         for (int j = 0 ; j < 28 ; ++j){
-            out << setw(3) << left << images[n][i*28 + j];
+            out << setw(3) << left << (int)images[n*28*28 + i*28 + j];
         }
         out << endl;
     }
 }
-void KNN(){
+
+int showLabel(int n){
+    return (int)labels[n];; 
+}
+
+void showProgress(int n){
+    if (n % (TEST_SET_SIZE / 10) == 0)
+        cout << n / (TEST_SET_SIZE / 10) * 10 << "%" << endl;
+}
+
+void KNN(ostream &out){
     int K = 5; // K as of K-nearest neighbour
-    int N = 10; // Number of test
     int occurrence[10];
     int predict;
-    long long distance = 0;
-    pair<long long, int> nearest[K]; // <distance, label> 
+    double distance;
+    pair<double, int> nearest[K]; // <distance, label> 
     
 
 
     for (int n = S_TEST ; n < T_TEST ; ++n){
+        // Show Image
+        //showImage(out, n);
+        
+        // Show progress
+        //showProgres(n);
+
         // Init 
         memset(occurrence, 0, sizeof(occurrence));
         for (int i = 0 ; i < K ; ++i) nearest[i] = {INF, -1};
@@ -58,8 +100,8 @@ void KNN(){
         for (int k = S_TRAINING ; k < T_TRAINING ; ++k){
             // Calculate distance from images[n] to images[k]
             distance = 0;
-            for (int i = 0 ; i < 28*28 ; ++i) distance += abs(images[n][i] - images[k][i]); 
-            cout << distance << endl;
+            for (int i = 0 ; i < 28*28 ; ++i) 
+                distance += abs(images[n*28*28+i] - images[k*28*28+i]); 
 
             // If images[k] is nearer, add it to the list
             for (int i = 0 ; i < K ; ++i){
@@ -87,11 +129,20 @@ void KNN(){
                 predict = i;
             }
         }
-        out << predict << endl;
+        out << "Predict: " << setw(8) << left << predict << "Result: " << (predict == showLabel(n) ? "Correct" : "Wrong") << "\n\n";
+        correct += predict == showLabel(n);
     }
 }
+
+void reportResult(ostream &out){
+    out << "No. of correct: " << correct << '/' << TEST_SET_SIZE << endl;
+    out << "Error rate: " << 1 - correct*1.0/TEST_SET_SIZE << endl;
+}
+
 int main(){
     readData();
-    KNN();
+    KNN(details);
+    reportResult(out);
+    cout << "DONE" << endl;
     return 0;
 }
---
+++ KNN/README.md
@@ -0,0 +1,6 @@
+# Handwritten Digit Recognition
+By recognizing the whole handwriting words, we first attempt to recognize single digit first.
+
+We use [MNIST](http://yann.lecun.com/exdb/mnist/) database with K-Nearest Neighbor algorithms to solve the stated problem.
+
+Please extract ubyte files before running the KNN algorithm
