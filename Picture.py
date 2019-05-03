cce2882631f3d7e661774e5a8ee5ed346e0f7e7d
Nguyen Minh Dang <dang.nguyen.bkit@hcmut.edu.vn>
2019-05-03 12:10:24 +0700

    Add Picture class This Picture class will convert image from opencv format to tkinter format

---
+++ Picture.py
@@ -0,0 +1,50 @@
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from PIL import Image, ImageTk
+import cv2
+import numpy as np
+
+class Picture:
+    def __init__(self, image_name, greyscale = True):
+        self.image = cv2.imread(image_name, not greyscale) 
+        if not greyscale:
+            b, g, r = cv2.split(self.image)
+            self.image = cv2.merge((r, g, b))
+        self.image = Image.fromarray(self.image)
+        self.image = ImageTk.PhotoImage(image=self.image) 
+
+
+if __name__ == "__main__":
+    def import_image():
+        filename = askopenfilename( initialdir = "./",
+                                    initialfile = "",
+                                    filetypes = (("All Files", "*"),
+                                                ("Text File", "*.txt")),
+                                    title = "Choose image")
+        if len(filename) > 0:
+            global img
+            global image
+            img = Picture(filename, greyscale.get()).image
+            image = Label(root, image = img)
+            image.grid(row = 1, column = 0, columnspan = 2, sticky = W) 
+
+    # Setup window
+    root = Tk()
+    root.config(bg = 'white')
+    root.title('Display opencv image')
+    root.geometry('500x500')
+    root.resizable(False, False)
+
+    greyscale = BooleanVar()
+
+    # Widgets
+    b_import    = Button(root, bg = 'white', text = 'Import image', command = import_image)
+    b_greyscale = Checkbutton(root, bg = 'white', variable = greyscale, text = 'Greyscale')
+    b_greyscale.select()
+
+    # Positioning
+    b_import.grid(row = 0, column = 0)
+    b_greyscale.grid(row = 0, column = 1)
+
+    # Main loop
+    root.mainloop()
