826f4d83d5f9477374567ab2ffa92ca5ed962074
Nguyen Minh Dang <dang.nguyen.bkit@hcmut.edu.vn>
2019-05-05 23:59:02 +0700

    Major updates Change Picture function to a class, now a Picture object with have 2 forms: matrix form and TkImage form Globalize all functions Change variables names for readability enhancement Fix bugs on update_ports function, handling the case when no port is opening Change window title

--- Picture.py
+++ Picture.py
@@ -7,20 +7,26 @@ import numpy as np
 import glob
 import sys
 
+class Picture:
+    def __init__(self, image_name, greyscale = True):
+        self.image_cv = cv2.imread(image_name, 0)
+        self.image_tk = cv2.imread(image_name, not greyscale)
+        if not greyscale:
+            b, g, r = cv2.split(self.image_tk)
+            self.image_tk = cv2.merge((r, g, b))
+        self.image_tk = Image.fromarray(self.image_tk)
+        self.image_tk = ImageTk.PhotoImage(image = self.image_tk)
 
-def Picture(image_name, greyscale = True):
-    image = cv2.imread(image_name, not greyscale)
-    if not greyscale:
-        b, g, r = cv2.split(image)
-        image = cv2.merge((r, g, b))
-    image = Image.fromarray(image)
-    image = ImageTk.PhotoImage(image=image)
-    return image
+    def mat(self):
+        return self.image_cv
+
+    def obj(self):
+        return self.image_tk
+
 def available_ports():
     if sys.platform.startswith('win'):
         ports = ['COM%s' % (i + 1) for i in range(256)]
     elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
         ports = glob.glob('/dev/tty[A-Za-z]*')
     elif sys.platform.startswith('darwin'):
         ports = glob.glob('/dev/tty.*')
@@ -36,69 +42,79 @@ def available_ports():
         except (OSError, serial.SerialException):
             pass
     return result
-if __name__ == "__main__":
-    def import_image():
-        filename = askopenfilename( initialdir = "./",
-                                    initialfile = "",
-                                    filetypes = (("All Files", "*"),
-                                                ("Text File", "*.txt")),
-                                    title = "Choose image")
-        if len(filename) > 0:
-            global img
-            global image
-            img = Picture(filename, greyscale.get())
-            image.config(image = img)
-  
-    def push_image():
-        img = img.reshape(img.shape[0]*img.shape[1])
-        ser = serial.Serial(port='', baudrate=115200)
-        ser.write(img)
-        
-    def updatePort(chosenPort):
+
+def import_image():
+    filename = askopenfilename( initialdir = "./",
+                                initialfile = "",
+                                filetypes = (("All Files", "*"),
+                                            ("Text File", "*.txt")),
+                                title = "Choose image")
+    if len(filename) > 0:
         try:
-            s = serial.Serial(chosenPort)
-            s.close()
-        except (OSError, serial.SerialException):
-            listPorts = available_ports()
-            
+            global subject 
+            global image
+            subject = Picture(filename, greyscale.get())
+            image.config(image = subject.obj())
+        except:
+            print("Invalid image type")
+  
+def push_image():
+    try:
+        data = subject.mat()
+        data = data.reshape(data.shape[0]*data.shape[1])
+        ser = serial.Serial(port = selected_port.get(), baudrate=115200)
+        ser.write(data)
+    except:
+        print("An error occured. Please try again.")
+ 
+def update_ports(event):
+    port_list = available_ports()
+    tmp = port_menu["menu"]
+    tmp.delete(0, "end")
+    if len(port_list) == 0: port_list.append("None")
+    for port in port_list: tmp.add_command(label = port)
+    selected_port.set(port_list[0])
+
+if __name__ == "__main__":
     # Setup window
     root = Tk()
     root.config(bg = 'white')
-    root.title('Display opencv image')
+    root.title('Zedboard communicator')
     root.geometry('500x500')
-    root.resizable(False, False)
+    #root.resizable(False, False)
 
     # Variables
     greyscale = BooleanVar()
-    option = StringVar(root)
-    ports = available_ports()
-    if len(ports) == 0: ports.append("None")
-    option.set(ports[0])
+    selected_port = StringVar(root)
+    port_list = available_ports()
+    if len(port_list) == 0: port_list.append("None")
+    selected_port.set(port_list[0])
     
-    # Port list
-    listPorts = available_ports()
-    optionMenu = OptionMenu(root, option, *listPorts, command=updatePorts)
-    optionMenu.grid(row=2, column=0)
 
+    port_menu   = OptionMenu(root, selected_port, *port_list)
+    port_menu.config(bg = "white", width = 10)
+    port_menu.bind("<Button-1>", update_ports)
+
+    image       = Label(root)
+    b_import    = Button(root, bg = 'white', text = 'Import image', command = import_image)
+    b_greyscale = Checkbutton(root, bg = 'white', variable = greyscale, text = 'Greyscale')
+    b_push      = Button(root, bg='white', text='Push', command = push_image)
+
+    b_greyscale.select()
+    
     # Display result
     result = 5
-    text = Text(root, width=20, height=10, font=("Helvetica", 25))
+    text = Text(root, width=15, height=8)
     text.insert('1.0', 'Result: ')
     text.insert('2.0', result)
-    
-    # Widgets
-    image       = Label(root)
-    b_import    = Button(root, bg = 'white', text = 'Import image', command = import_image, font=("Helvetica", 19))
-    b_greyscale = Checkbutton(root, bg = 'white', variable = greyscale, text = 'Greyscale', font=("Helvetica", 19))
-    b_greyscale.select()
-    b_push = Button(root, bg='white', text='Push', command = push_image)
 
     # Positioning
-    optionMenu.grid(row=0, column=0)
+    port_menu.grid(row = 0, column = 0)
     b_import.grid(row = 0, column = 1)
     b_push.grid(row=0, column = 2, sticky = W)
     b_greyscale.grid(row = 0, column = 3)
-    image.grid(row = 1, column = 0, columnspan = 2, sticky = W) 
+    image.grid(row = 1, column = 0, sticky = W) 
+    text.grid(row = 1, column = 1)
 
     # Main loop
     root.mainloop()
