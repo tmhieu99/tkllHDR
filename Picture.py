03793c4ecf6f30f1159db9b5c1a55fd16d89a764
Nguyen Minh Dang <dang.nguyen.bkit@hcmut.edu.vn>
2019-05-05 17:26:14 +0700

    Fix minor image bug

--- Picture.py
+++ Picture.py
@@ -11,10 +11,9 @@ def Picture(image_name, greyscale = True):
     image = cv2.imread(image_name, not greyscale) 
     if not greyscale:
         b, g, r = cv2.split(image)
-        self.image = cv2.merge((r, g, b))
+        image = cv2.merge((r, g, b))
     image = Image.fromarray(image)
     image = ImageTk.PhotoImage(image=image) 
-    print(list(image))
     return image
 
 def available_ports():
@@ -49,8 +48,7 @@ if __name__ == "__main__":
             global img
             global image
             img = Picture(filename, greyscale.get())
-            image = Label(root, image = img)
-            image.grid(row = 1, column = 0, columnspan = 2, sticky = W) 
+            image.config(image = img)
 
     # Setup window
     root = Tk()
@@ -59,16 +57,23 @@ if __name__ == "__main__":
     root.geometry('500x500')
     root.resizable(False, False)
 
+    # Variables
     greyscale = BooleanVar()
+    option = StringVar(root)
+    ports = available_ports()
+    if len(ports) == 0: ports.append("None")
+    option.set(ports[0])
 
     # Widgets
+    image       = Label(root)
     b_import    = Button(root, bg = 'white', text = 'Import image', command = import_image)
     b_greyscale = Checkbutton(root, bg = 'white', variable = greyscale, text = 'Greyscale')
     b_greyscale.select()
 
     # Positioning
-    b_import.grid(row = 0, column = 0)
-    b_greyscale.grid(row = 0, column = 1)
+    b_import.grid(row = 0, column = 1)
+    b_greyscale.grid(row = 0, column = 2)
+    image.grid(row = 1, column = 0, columnspan = 2, sticky = W) 
 
     # Main loop
     root.mainloop()
