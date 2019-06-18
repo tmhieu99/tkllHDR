419276be0d08d5198b6556f21d8a797fabc2cbb2
Nguyen Minh Dang <dang.nguyen.bkit@hcmut.edu.vn>
2019-06-18 12:32:23 +0700

    Get MLP weights

--- src/models/MLP.py
+++ src/models/MLP.py
@@ -1,5 +1,5 @@
 import numpy as np
-import keras
+import keras 
 from keras.datasets import mnist
 from keras.layers import Dense, Flatten
 from keras.models import Sequential
@@ -21,8 +21,20 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 x_train = x_train.reshape(x_train.shape[0], 28*28).astype('float32')
 x_test = x_test.reshape(x_test.shape[0], 28*28).astype('float32')
 
-print("hello")
 # Define MLP model
+def print_weights(model):
+    a = model.get_weights()
+    for i in range(len(a)):
+        File = open("layer" + str(i) + ".csv", "w")
+        for j in range(len(a[i])):
+            try:
+                for k in range(len(a[i][j])):
+                    File.write("{0:0.25f}, ".format(a[i][j][k]))
+                File.write("\n")
+            except:
+                File.write("{0:0.25f}, \n".format(a[i][j]))
+        File.close()
+
 def MLP(num_layers = 1, num_units = 1):
     # Create layers
     model = Sequential()
@@ -67,6 +79,5 @@ Instruction:
 #   in each layer.                                                                        #
 #                                                                                         #
 ###########################################################################################
-
 """
 )
