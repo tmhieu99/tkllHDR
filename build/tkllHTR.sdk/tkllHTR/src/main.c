2b55a3e38a3ed676c265f4a1051152f4219402e4
dangne <dang.nguyen.bkit@hcmut.edu.vn>
2019-06-24 21:21:54 +0700

    Replace malloc paradigm with static variables

--- build/tkllHTR.sdk/tkllHTR/src/main.c
+++ build/tkllHTR.sdk/tkllHTR/src/main.c
@@ -1,11 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: Jun 16, 2019
+ *      Author: dangn
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "xil_printf.h"
 #include "sleep.h"
 #include "mlp.h"
-#include "matrix.h"
+#include "operations.h"
 
-#define INPUT_SIZE			10
+#define INPUT_SIZE			784
 #define BUFFER_SIZE 		(INPUT_SIZE*3 + INPUT_SIZE)
 #define IDLE_STATE 			1
 #define EXECUTION_STATE 	2
@@ -13,49 +21,87 @@
 
 int main(){
 	char buffer[BUFFER_SIZE];
-	char* token;
 	int state = IDLE_STATE;
-	int result = -1;
-	int cnt;
-	Matrix input = new_matrix(1, INPUT_SIZE);
+	int result;
+    double input[1][INPUT_SIZE];
+    char* token;
 
 	while (1) {
 		switch (state) {
 			case IDLE_STATE:
 				gets(buffer);
-				xil_printf("Input: %s\n",buffer);
 				state = EXECUTION_STATE;
 				break;
 				
 			case EXECUTION_STATE:
-				token = strtok(buffer, ",\n");
+				token = strtok(buffer, ",");
 				int i = 0;
-				cnt = 0;
 				while (token != NULL){
-					cnt++;
-					input.at[0][i] = 0;
+					input[0][i] = 0;
 					for (int j = 0 ; j < strlen(token) ; ++j)
-						input.at[0][i] = input.at[0][i]*10 + token[j] - '0';
-					token = strtok(NULL, ",\n");
+						input[0][i] = input[0][i]*10 + token[j] - '0';
+					token = strtok(NULL, ",");
 					i++;
 				}
-				/*
+
 				// Normalize input to 0-1
 				for (int i = 0 ; i < INPUT_SIZE ; ++i)
-					input.at[0][i] /= 255.0;
-				*/
-				//result = MLP(input);
+					input[0][i] /= 255.0;
+
+
+
+
+
+
+
+				/*----------------- FOR DEBUGGING PURPOSE -----------------*/
+				//MLP(input);
+				xil_printf("Begin MLP\n");
+
+
+
+
+
+				double w0[UNIT_LAYER_INPUT][UNIT_LAYER_1] 	= WEIGHT_0;
+				double b0[1][UNIT_LAYER_1] 					= BIAS_0;
+				double w1[UNIT_LAYER_1][UNIT_LAYER_OUTPUT] 	= WEIGHT_1;
+				double b1[1][UNIT_LAYER_OUTPUT] 			= BIAS_1;
+
+				double hidden[1][UNIT_LAYER_1];
+				MUL(hidden, input,  w0);
+				ADD(hidden, hidden, b0);
+				RELU(hidden);
+
+				double output[1][UNIT_LAYER_OUTPUT];
+				MUL(output, hidden, w1);
+				ADD(output, output, b1);
+				RELU(output);
+				SOFTMAX(output);
+
+				result = 0;
+				for (int i = 0 ; i < LEN(output[0]) ; i++)
+					if (output[0][i] > output[0][result])
+						result = i;
+
+
+
+
+
+
+				xil_printf("End MLP\n");
+
+				/*----------------- FOR DEBUGGING PURPOSE -----------------*/
+
+
+
+
+
+
 				state = RESULT_STATE;
 				break;
 
 			case RESULT_STATE:
-				xil_printf("Number of numbers: %d\n", cnt);
-				xil_printf("Output: ");
-				for (int i = 0 ; i < cnt ; ++i){
-					if (i != 0) xil_printf(",");
-					xil_printf("%d", (int)input.at[0][i]);
-				}
-				xil_printf("\n");
+				xil_printf("Result: %d\n", result);
 				state = IDLE_STATE;
 				break;
 
--- build/tkllHTR/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_tkllhtr.elf_on_local.tcl
+++ build/tkllHTR/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_tkllhtr.elf_on_local.tcl
@@ -12,5 +12,4 @@ rst -processor
 targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zed 210248782650"} -index 0
 dow C:/Users/dangn/Dropbox/it/CodingSpace/tkllHTR/build/tkllHTR.sdk/tkllHTR/Debug/tkllHTR.elf
 configparams force-mem-access 0
-targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zed 210248782650"} -index 0
-con
+bpadd -addr &main
