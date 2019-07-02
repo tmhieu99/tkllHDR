72e394209d9859d917b8f049298e150dd88581bb
dangne <dang.nguyen.bkit@hcmut.edu.vn>
2019-07-02 23:23:23 +0700

    Cherry pick from debug-malloc branch

---
+++ build/tkllHTR.sdk/tkllHTR/src/main.h
@@ -0,0 +1,19 @@
+/*
+ * main.h
+ *
+ *  Created on: Jul 2, 2019
+ *      Author: dangn
+ */
+
+#ifndef SRC_MAIN_H_
+#define SRC_MAIN_H_
+
+#define IDLE_STATE 			1
+#define EXECUTION_STATE 	2
+#define RESULT_STATE 		3
+#define LAYER_INPUT			196
+#define LAYER_1				64
+#define LAYER_OUTPUT		10
+#define BUFFER_SIZE 		(LAYER_INPUT*3 + LAYER_INPUT)
+
+#endif /* SRC_MAIN_H_ */
